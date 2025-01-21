26c0b40adccd06ac491a9e9e1692b80a87882ff1
Andrey Rubio <153574108+Minolistya@users.noreply.github.com>
2025-01-21 18:57:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript functionality for the button
+document.getElementById("clickMe").addEventListener("click", function () {
+    alert("Button clicked!");
+    console.log("Button was clicked!");
+  });
